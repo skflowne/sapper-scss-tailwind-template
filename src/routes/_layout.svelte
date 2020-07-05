5ab9d8f653b2760df66c46586a1e7de30cdcbef4
Geoffrey Hug <hug.geoffrey@gmail.com>
2020-07-05 22:17:41 +0200

    Add config for VS Code Svelte extension

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,6 +2,17 @@
   //export let segment;
 </script>
 
+<style lang="scss">
+  main {
+    .test {
+      width: 500px;
+      height: 500px;
+      background: red;
+    }
+  }
+</style>
+
 <main>
+  <div class="test" />
   <slot />
 </main>
